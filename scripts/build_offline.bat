41283e908706e98c526e040abaa8907de6a3653e
Jan Madsen <janm@comcon.dk>
2025-11-11 10:39:57 +0100

    Added scrub_csv.

--- scripts/build_offline.bat
+++ scripts/build_offline.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-py -v3.11 -m venv .venv
+py -3.11 -m venv .venv
 call .venv\Scripts\activate
 py -m pip install --no-index --find-links=.\wheels -r requirements.txt
 echo Install complete.
